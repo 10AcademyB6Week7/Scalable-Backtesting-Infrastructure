f7545be8ba2f9d8abbc8e436043c9971d3ecf990
Gedion <gedionandme@gmail.com>
2022-10-11 22:59:59 +0300

    Implemented kafka topic creator.

---
+++ scripts/create_kafka_topics.py
@@ -0,0 +1,16 @@
+from kafka import KafkaAdminClient 
+from kafka.admin import NewTopic
+
+
+def create_topics(topic_list):
+    client = KafkaAdminClient(
+            bootstrap_servers = ['b-1.batch6w7.6qsgnf.c19.kafka.us-east-1.amazonaws.com:9092','b-2.batch6w7.6qsgnf.c19.kafka.us-east-1.amazonaws.com:9092'],
+            api_version=(0,11,5),)
+    
+    new_topic = []
+    
+    for topic in topic_list:
+        new_topic.append(NewTopic(name=topic,num_partitions=3,replication_factor=1))
+    client.create_topics(new_topics=new_topic,validate_only=False)
+    
+    return client.list_topics()
