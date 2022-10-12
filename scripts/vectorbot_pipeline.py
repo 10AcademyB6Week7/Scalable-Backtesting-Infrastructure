c22678970001b285f40d6ea4b3ce163f32e08252
Natnael Melese <natrix277@gmail.com>
2022-10-12 11:48:37 +0000

    Vectorbot some plots and results

--- scripts/vectorbot_pipeline.py
+++ scripts/vectorbot_pipeline.py
@@ -2,7 +2,7 @@ import vectorbt as vbt
 
 
 class VectorbotPipeline():
-    def __init__(self, init_cash=100, stock='LTC-USD', fast_ma=10, slow_ma=50, start='2021-10-10', end='2022-10-10', period=None, fees=0.005):
+    def __init__(self, init_cash=1000, stock='AMZN', fast_ma=10, slow_ma=50, start='2021-10-11', end='2022-10-11', period=None, fees=0.005, is_experiment=False):
         self.init_cash = init_cash
         self.stock = stock
         self.fast_ma = fast_ma
@@ -11,6 +11,7 @@ class VectorbotPipeline():
         self.end = end
         self.period = period
         self.fees = fees
+        self.is_experiment = is_experiment
         self.price = vbt.YFData.download(self.stock, start=self.start, end=self.end).get('Close')
 
     def setup_from_holding(self):
@@ -28,14 +29,27 @@ class VectorbotPipeline():
         print(self.pf.orders.records_readable)
         print(self.pf.total_profit())
 
-    def plot(self):
+    def plot_fast_and_slow(self):
         fig = self.price.vbt.plot(trace_kwargs=dict(name='Close'))
         self.calc_fast_ma.ma.vbt.plot(trace_kwargs=dict(name='Fast MA'), fig=fig)
         self.calc_slow_ma.ma.vbt.plot(trace_kwargs=dict(name='Slow MA'), fig=fig)
         self.pf.positions.plot(close_trace_kwargs=dict(visible=False), fig=fig)
         # vbt.save('fig.png', fig)
-        with open('img.png','wb') as f:
+        with open('./images/vectorbot/fast_and_slow_plot.png','wb') as f:
             f.write(fig.to_image(format='png'))
+
+    def return_backtest_result(self):
+        return self.pf.stats().to_dict()
+
+    # def plot_portfolio_summary(self):
+    #     returns = self.price.vbt.to_returns()
+    #     val = returns.vbt.returns.qs.html_report
+    #     # val = returns.vbt.returns.qs.plot_snapshot()
+    #     # print(val)
+    #     # print(type(val))
+    #     object_methods = [method_name for method_name in dir(val)
+    #               if callable(getattr(val, method_name))]
+    #     print(object_methods)
 
     
 
@@ -44,6 +58,10 @@ if __name__ == '__main__':
     vbtp = VectorbotPipeline()
     vbtp.setup_sma()
     vbtp.readbale_records()
-    vbtp.plot()
+    vbtp.plot_fast_and_slow()
+    vbtp.return_backtest_result()
+    # vbtp.plot_portfolio_summary()
     vbtp.setup_from_holding()
-    vbtp.readbale_records()
+    # vbtp.readbale_records()
+    vbtp.return_backtest_result()
+
