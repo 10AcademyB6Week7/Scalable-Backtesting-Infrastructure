e58226469f98765e4623b438ccce7dad9c17643f
Natnael Melese <natrix277@gmail.com>
2022-10-12 14:09:11 +0000

    MLFlow init

--- scripts/vectorbot_pipeline.py
+++ scripts/vectorbot_pipeline.py
@@ -1,5 +1,6 @@
 import vectorbt as vbt
-
+from mlflow import log_metric, log_param, log_artifacts, log_params
+import mlflow
 
 class VectorbotPipeline():
     def __init__(self, init_cash=1000, stock='AMZN', fast_ma=10, slow_ma=50, start='2021-10-11', end='2022-10-11', period=None, fees=0.005, is_experiment=False):
@@ -13,11 +14,27 @@ class VectorbotPipeline():
         self.fees = fees
         self.is_experiment = is_experiment
         self.price = vbt.YFData.download(self.stock, start=self.start, end=self.end).get('Close')
+        if(is_experiment):
+            try:
+                mlflow.end_run()
+            except:
+                pass
+            mlflow.set_experiment("self_stock")
+            mlflow.set_tracking_uri('http://localhost:5454')
+            mlflow.start_run(run_name="self_stock")
 
     def setup_from_holding(self):
+        if self.is_experiment:
+            log_param("stock", self.stock)
+            log_param("init_cash", self.init_cash)
         self.pf = vbt.Portfolio.from_holding(self.price, init_cash=self.init_cash)
     
     def setup_sma(self):
+        if self.is_experiment:
+            log_param("stock_sma", self.stock)
+            log_param("stock_fast_sma", self.fast_ma)
+            log_param("stock_slow_sma", self.slow_ma)
+            log_param("init_cash", self.init_cash)
         # price = vbt.YFData.download(self.stock, start=self.start, end=self.end).get('Close')
         self.calc_fast_ma = vbt.MA.run(self.price, self.fast_ma, short_name='fast_ma')
         self.calc_slow_ma = vbt.MA.run(self.price, self.slow_ma, short_name='slow_ma')
@@ -37,8 +54,15 @@ class VectorbotPipeline():
         # vbt.save('fig.png', fig)
         with open('./images/vectorbot/fast_and_slow_plot.png','wb') as f:
             f.write(fig.to_image(format='png'))
+        if self.is_experiment:
+            mlflow.log_figure(fast_and_slow_plot, "fast_and_slow_plot.png")
 
     def return_backtest_result(self):
+        if self.is_experiment:
+            log_param("stock", self.stock)
+            log_param("init_cash", self.init_cash)
+            for k,v in self.pf.stats().to_dict():
+                log_param(k,v)
         return self.pf.stats().to_dict()
 
     # def plot_portfolio_summary(self):
@@ -55,7 +79,7 @@ class VectorbotPipeline():
 
 
 if __name__ == '__main__':
-    vbtp = VectorbotPipeline()
+    vbtp = VectorbotPipeline(is_experiment=True)
     vbtp.setup_sma()
     vbtp.readbale_records()
     vbtp.plot_fast_and_slow()
