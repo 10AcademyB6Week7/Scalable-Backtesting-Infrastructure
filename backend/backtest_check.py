f677eaeaf0df17f81f5b7fd1fe06a3f3dab756e4
Gedion <gedionandme@gmail.com>
2022-10-15 21:56:54 +0000

    Backtest checker update

--- backend/backtest_check.py
+++ backend/backtest_check.py
@@ -7,6 +7,7 @@ sys.path.insert(0,'../scripts/')
 from create_kafka_topics import create_topics
 from kafka_producer import producer
 from kafka_consumer import consumer
+from vectorbt_pipeline import VectorbotPipeline
 from models import BackTestScene,BackTestResult
 logging.basicConfig(filename='../log/log.log', filemode='a',encoding='utf-8', level=logging.DEBUG)
 
@@ -33,4 +34,5 @@ def check_backtest():
             create_topics([user_ID])
             return producer('backtest_producer',user_ID,{"returns":response.returns,"number_of_trades":response.number_of_trades,"winning_trades":response.winning_trades,"losing_trades":response.losing_trades,"max_drawdown":response.max_drawdown,"sharpe_ratio":response.sharpe_ratio})
         elif check is None:
-            pass
+            result = VectorbotPipeline(user_id=user_ID,start=start_date,end=end_date,slow_ma=sma_value,fast_ma=fma_value,init_cash=inital_cash,stock=coin_name)
+            return result.save_result_and_publish()
